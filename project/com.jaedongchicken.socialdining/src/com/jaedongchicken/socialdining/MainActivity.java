2a98d6f1b804b8176ff3b8bd777d071a9d72cf08
ryancho <chowin21@gmail.com>
2014-04-20 10:56:20 +0900

    1

--- project/com.jaedongchicken.socialdining/src/com/jaedongchicken/socialdining/MainActivity.java
+++ project/com.jaedongchicken.socialdining/src/com/jaedongchicken/socialdining/MainActivity.java
@@ -1,16 +1,95 @@
 package com.jaedongchicken.socialdining;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity{
+import com.sromku.simple.fb.SimpleFacebook;
+import com.sromku.simple.fb.listeners.OnLogoutListener;
+
+public class MainActivity extends Activity {
+
+	// begin declare 
+	private Button mButtonLogout;
+	private SimpleFacebook mSimpleFacebook;
 	
-	public void onCreate(Bundle savedInstanceState) {
+	
+	// Logout listener
+	private OnLogoutListener mOnLogoutListener = new OnLogoutListener() {
+
+		@Override
+		public void onFail(String reason) {
+		}
+
+		@Override
+		public void onException(Throwable throwable) {
+		}
+
+		@Override
+		public void onThinking() {
+			// show progress bar or something to the user while login is
+			// happening
+		}
+
+		@Override
+		public void onLogout() {
+			// change the state of the button or do whatever you want
+			setUIState();
+		}
+
+	};
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mSimpleFacebook = SimpleFacebook.getInstance(this);
+		setUIState();
+	}
+	
+	private void setUIState() {
+		if (mSimpleFacebook.isLogin()) {
+			
+			
+		} else {
+			//loggedOutUIState();
+			Intent intent = new Intent();
+			intent.setClass(getApplicationContext(), LoginActivity.class);
+			// intent.putExtra("EXTRA_ID", "SOME DATAS");
+			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			
+		}
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//Toast.makeText(getApplicationContext(), "msg msg", Toast.LENGTH_LONG).show();
-		
-	};
+
+		initUI();
+
+
+		// 2. Logout example
+		logoutExample();
+	}
+
 	
+
+	private void initUI() {
+		mButtonLogout = (Button) findViewById(R.id.button_logout);
+	}
+	
+	/**
+	 * Logout example
+	 */
+	private void logoutExample() {
+		mButtonLogout.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View arg0) {
+				mSimpleFacebook.logout(mOnLogoutListener);
+			}
+		});
+	}
 }
